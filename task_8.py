4c0742d5074dd836d19409c8a9be9b2b2b3baf65
Ladygin <ladygin.nikitaa@gmail.com>
2024-12-02 19:33:22 +0300

    task_8

--- task_8.py
+++ task_8.py
@@ -3,6 +3,7 @@ chipertext = chipertext.split()
 ROWS = 5
 COLS = 4
 KEY = '-1 2 -3 4'
+key_int = [int(x) for x in KEY.split()]
 translation_matrix = [None] * COLS
 start = 0
 stop = ROWS
@@ -10,4 +11,16 @@ for i in range(COLS):
     translation_matrix[i] = chipertext[start:stop]
     start = stop 
     stop += ROWS
-print(translation_matrix)
+print(*translation_matrix, sep='\n')
+print()
+for idx, key in enumerate(key_int):
+    if key < 0:
+        translation_matrix[idx] = translation_matrix[idx][::-1]
+print(*translation_matrix, sep='\n')
+
+result_text = ''
+for i in range(COLS):
+    for item in translation_matrix:
+        word = item[i]
+        result_text += word + ' '
+print(result_text)
